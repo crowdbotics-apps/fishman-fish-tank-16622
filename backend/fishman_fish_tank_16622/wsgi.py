8a75703fcc669500ac7f584f6ec2c6b935cf5beb
Crowdbotics <team@crowdbotics.com>
2020-05-05 14:56:39 +0000

    Initial commit.

---
+++ backend/fishman_fish_tank_16622/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fishman_fish_tank_16622 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fishman_fish_tank_16622.settings')
+
+application = get_wsgi_application()
